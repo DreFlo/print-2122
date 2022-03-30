8e2b6919a6bf50dd9654c0fc14183958da445278
Andre Flores <andrejesusferflores@gmail.com>
2022-03-30 13:52:02 +0100

    User can now choose CSV filename

--- _engine/csv_table.py
+++ _engine/csv_table.py
@@ -16,8 +16,8 @@ def get_table_html():
 def main():
     table_html = get_table_html()
     df = pd.read_html(table_html)[0]
-    path = filedialog.askdirectory()
-    df.to_csv(path + '/table.csv', index=False)
+    filepath = filedialog.asksaveasfilename(defaultextension='.csv', filetypes=[('CSV', '*.csv')], title='Choose filename')
+    df.to_csv(filepath, index=False)
 
     jsonObject = BuildJson({})
     print(jsonObject.getJson())
