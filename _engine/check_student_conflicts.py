4d49b99ada95cdce37826a26fed4e2578ea412a2
Andre Flores <andrejesusferflores@gmail.com>
2022-04-05 20:48:04 +0100

    Work on multiprocessing

--- _engine/check_student_conflicts.py
+++ _engine/check_student_conflicts.py
@@ -2,19 +2,22 @@ import Core
 from handle_json import BuildJson
 import sys
 from functools import reduce
+import multiprocessing as mp
 
 def get_UC_codes():
     codes_string =  sys.argv[1]
     return codes_string.split(sep=";")
 
+def get_UC_students_set(code):
+    return set(Core.get_curricular_unit_students(code))
+
 codes = get_UC_codes()
 
 response =  {'name':'UCConflict'}
 
-studentSets = []
+pool = mp.Pool(len(codes))
 
-for code in codes:
-    studentSets.append(set(Core.get_curricular_unit_students(code)))
+studentSets = pool.map(get_UC_students_set, codes)
 
 commonStudent = reduce(lambda x,y : x & y, studentSets)
 
